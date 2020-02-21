ab87c581807db49194774efeb027370f99d104cc
Sofia Silva <up201505628@fe.up.pt>
2020-02-21 03:02:43 +0000

    hover animation on experience

--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -15,33 +15,33 @@ class Experience extends React.Component {
             <span>02.</span>Experience
           </h3>
           <div className="card-list">
-            <Card>
-              <Card.Header>Nov 2018 - Present</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Active Member{" "}
-                  <a href="https://up.ieee-pt.org">IEEE UP Student Branch</a>
-                </Card.Title>
-                <Card.Text>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Nov 2018 - Present</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Active Member{" "}
+                    <a href="https://up.ieee-pt.org">IEEE UP Student Branch</a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Prototype design and web development, working mostly for
                       the design and marketing department.
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>Jun 2017 - Present</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Web Developer, Marketing Manager and Designer{" "}
-                  <a href="http://portocampustudios.com">
-                    Porto Campus Studios
-                  </a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Jun 2017 - Present</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Web Developer, Marketing Manager and Designer{" "}
+                    <a href="http://portocampustudios.com">
+                      Porto Campus Studios
+                    </a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Defined the branding of the company by choosing a name and
@@ -53,19 +53,19 @@ class Experience extends React.Component {
                       Responsible for marketing and social media management.
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>Jul 2019</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Teaching Assistant{" "}
-                  <a href="https://universidadejunior.up.pt">
-                    Universidade Júnior
-                  </a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Jul 2019</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Teaching Assistant{" "}
+                    <a href="https://universidadejunior.up.pt">
+                      Universidade Júnior
+                    </a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Universidade Júnior is a summer course program at the
@@ -77,19 +77,19 @@ class Experience extends React.Component {
                       Taught programming to children from the age of 11 to 14.
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>Apr 2019</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Speaker in Semana Profissão Engenheiro{" "}
-                  <a href="https://paginas.fe.up.pt/~escolas/">
-                    Faculdade de Engenharia da Universidade do Porto
-                  </a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Apr 2019</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Speaker in Semana Profissão Engenheiro{" "}
+                    <a href="https://paginas.fe.up.pt/~escolas/">
+                      Faculdade de Engenharia da Universidade do Porto
+                    </a>
+                  </Card.Title>
                   <ul>
                     <li>
                       I was invited by a teacher to present Sweven, a project
@@ -100,17 +100,17 @@ class Experience extends React.Component {
                       regarding the course.
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>Apr 2019</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Promoter in Mostra da Universidade do Porto 2019{" "}
-                  <a href="https://www.mostra.up.pt">Universidade do Porto</a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Apr 2019</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Promoter in Mostra da Universidade do Porto 2019{" "}
+                    <a href="https://www.mostra.up.pt">Universidade do Porto</a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Promotion of FEUP's Master Degree in Informatics and
@@ -121,49 +121,49 @@ class Experience extends React.Component {
                       vocational decision.
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>Jan 2017 - Nov 2018</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Active Member <a href="https://ni.fe.up.pt">NIAEFEUP</a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Jan 2017 - Nov 2018</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Active Member <a href="https://ni.fe.up.pt">NIAEFEUP</a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Active participation in the Informatics Club of the
                       Faculty of Engineering (Academic Organization).
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>May 2018 - Sep 2018</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Active Member <a href="https://ni.fe.up.pt">NIAEFEUP</a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>May 2018 - Sep 2018</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Active Member <a href="https://ni.fe.up.pt">NIAEFEUP</a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Active participation in the Informatics Club of the
                       Faculty of Engineering (Academic Organization).
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Header>Jan 2017 - Jul 2018</Card.Header>
-              <Card.Body>
-                <Card.Title>
-                  Head of Design, Marketing and Communications{" "}
-                  <a href="https://sinf.pt">Semana de Informática</a>
-                </Card.Title>
-                <Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="card-item">
+              <Card>
+                <Card.Header>Jan 2017 - Jul 2018</Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    Head of Design, Marketing and Communications{" "}
+                    <a href="https://sinf.pt">Semana de Informática</a>
+                  </Card.Title>
                   <ul>
                     <li>
                       Participation in the organization of Semana de
@@ -171,9 +171,9 @@ class Experience extends React.Component {
                       the Faculty of Engineering at University of Porto.
                     </li>
                   </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
         </ScrollAnimation>
       </section>
